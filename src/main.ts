01552d0f92c548a3574dd430090996743f71e368
H Z Wei <hzwei92@gmail.com>
2022-04-05 20:24:50 -0700

    change start url via .env

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { AppService } from './app.service';
+import 'dotenv/config'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const appService = app.get(AppService);
-  appService.init('https://www.reddit.com', 5);
+  appService.init(process.env.START_URL, 5);
   appService.crawl(0);
   await app.close()
 }
